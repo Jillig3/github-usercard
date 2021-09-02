4df1fafc3b6193bf0572753b566aa894ebfd1a0d
unknown <jacobillig@gmail.com>
2021-09-02 17:11:45 -0500

    mostly done

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -28,8 +28,19 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
-
+// const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd','bigknell'];
+// followersArray.forEach(user => {
+//   axios.get(`https://api.github.com/users/${user}`)
+//   .then(res => {
+//     console.log(res);
+//       const cardProfile = cardMaker(res.data)
+//     infoCards.appendChild(cardProfile)
+//     console.log('Alive');
+//   })
+//   .catch(err => {
+//     console.error(err);
+//   })
+// });
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -50,6 +61,74 @@ const followersArray = [];
     </div>
 */
 
+
+const cardDiv = document.querySelector('.cards');
+
+function cardMaker( avatar_url, name, login, location, html_url, followers, following, bio) {
+
+  const card = document.createElement('div');
+  const cardImg = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const cardName = document.createElement('h3');
+  const cardUsername = document.createElement('p');
+  const cardLocation = document.createElement('p');
+  const cardProfile = document.createElement('p');
+  const cardA = document.createElement('a');
+  const cardFollowers = document.createElement('p');
+  const cardFollowing = document.createElement('p');
+  const cardBio = document.createElement('p');
+
+  card.appendChild(cardImg);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(cardName);
+  cardInfo.appendChild(cardUsername);
+  cardInfo.appendChild(cardLocation);
+  cardInfo.appendChild(cardProfile);
+  cardInfo.appendChild(cardFollowers);
+  cardInfo.appendChild(cardFollowing);
+  cardInfo.appendChild(cardBio);
+  cardProfile.appendChild(cardA);
+
+  card.className = "card";
+  cardInfo.className = "card-info";
+  cardName.className = "name";
+  cardUsername.className = "username";
+
+  cardImg.src = avatar_url;
+  cardInfo.textContent = (`${name}`);
+  cardUsername.textContent = `${login}`;
+  cardLocation.textContent = `Location: ${location}`;
+  cardProfile.textContent = `Profile: ${id}`;
+  cardA.href = html_url;
+  cardA.textContent = 'Check us out!';
+  cardFollowers.textContent = `Followers: ${followers}`;
+  cardFollowing.textContent = `Following: ${following}`;
+  cardBio.textContent = `Bio: ${bio}`;
+  
+  
+  return card;
+
+}
+
+// const user = Jillig3;
+
+// const CreateUserCard = async (user) => {
+//   try {
+//     const resp = await axios.gets(`https://api.github.com/users/${user}`); 
+//    for(let i = 0; i < resp.data.length; i++) {
+//     const userData = resp.data;
+//     const gitCard = cardMaker(userData);
+//     card.appendChile(gitCard);
+//    } 
+//   } 
+//   catch(err) {
+//     const errorText = document.createElement('p');
+//     errorText.textContent = "not working";
+//     document.body.appendChild(errorText);
+//   } finally {
+//     console.log('I hope this works');
+//   }
+// }
 /*
   List of LS Instructors Github username's:
     tetondan
@@ -58,3 +137,7 @@ const followersArray = [];
     luishrd
     bigknell
 */
+
+
+
+
